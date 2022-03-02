0ce1dd468ea5de9f9a1aee24fb6e5a4268e2c097
HenricoW <henrico.ashley.White@gmail.com>
2022-03-02 18:38:11 +0200

    formatting

--- src/assets/fbConfig.ts
+++ src/assets/fbConfig.ts
@@ -4,13 +4,13 @@
 //   databaseURL: "https://nuvest-1.firebaseio.com",
 //   projectId: "nuvest-1",
 //   storageBucket: "nuvest-1.appspot.com",
-//   messagingSenderId: "926977302380"
-// }
+//   messagingSenderId: "926977302380",
+// };
 export const firebaseConfig = {
   apiKey: "AIzaSyBbwvVwreAXcSVl1B3OWKWQ2-Cn_WIGLCE",
   authDomain: "awesome-app-2617f.firebaseapp.com",
   databaseURL: "https://awesome-app-2617f.firebaseio.com",
   projectId: "awesome-app-2617f",
   storageBucket: "awesome-app-2617f.appspot.com",
-  messagingSenderId: "334528911038"
-}
+  messagingSenderId: "334528911038",
+};
